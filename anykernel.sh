bb4d8d6f3271594b463e335b24544661ad3c8723
ZHYxulei <ZHYxulei@gmail.com>
2026-08-08 14:45:36 +0800

    customize anykernel.sh for miro-kernel-ultra
    - kernel.string=miro-kernel-ultra
    - do.devicecheck=0, do.modules=1
    - BLOCK=auto, IS_SLOT_DEVICE=1

--- anykernel.sh
+++ anykernel.sh
@@ -4,9 +4,9 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
-do.modules=0
+kernel.string=miro-kernel-ultra
+do.devicecheck=0
+do.modules=1
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
@@ -29,8 +29,8 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-IS_SLOT_DEVICE=0;
+BLOCK=auto;
+IS_SLOT_DEVICE=1;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
